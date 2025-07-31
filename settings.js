0e1c48d1a167a018ec5bdc94929344cb3b7ab4e8
nayan200325blip <nayan200325@gmail.com>
2025-07-31 13:15:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lucky~PEt30B7b#pUoTLAuIxfwTuzYPRRJLZUoK86gC-znc04qnOBwHgqA",
+SESSION_ID: process.env.SESSION_ID || "lucky~zZ1lybyL#Ep_raGuHlwc7mSl4uDHhehhlRpP-6PBBPU5CD2g6YzQ",
 // add your Session Id make sure it starts with lucky~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
